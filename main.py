c196def311e2829d864c6a7c9b290bcdf4276ed2
Rushabh <rushabhpatel100@gmail.com>
2019-12-21 18:22:44 +0530

    1.5

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 client1 = ModbusClient(method = 'rtu', port = '/dev/ttyUSB0', parity='E',stopbits=1,bytesize=8,baudrate = 9600,timeout=1)
 
-def up(start,stop,ramp):
+def up(start,stop,ramp,unit11):
     total = stop-start
     print total
     a = total/ramp #Hz/Second
@@ -11,15 +11,15 @@ def up(start,stop,ramp):
     print c
     while c <= total:
         print client1.connect()
-        t = client1.write_register(0x2001,start,unit=1)
-        l = client1.write_register(0x2001,start,unit=2)
+        t = client1.write_register(0x2001,start,unit=unit11)
+        #l = client1.write_register(0x2001,start,unit=2)
         start += a
         c += a
         print c
         time.sleep(1)
         client1.close()
 
-def down(start,stop,ramp):
+def down(start,stop,ramp,unit21):
     total = start-stop
     print total
     a = total/ramp #Hz/Second
@@ -28,8 +28,8 @@ def down(start,stop,ramp):
     print c
     while c <= total:
         print client1.connect()
-        t = client1.write_register(0x2001,start,unit=1)
-        l = client1.write_register(0x2001,start,unit=2)
+        t = client1.write_register(0x2001,start,unit=unit21)
+        #l = client1.write_register(0x2001,start,unit=2)
         start -= a
         c += a
         print c
@@ -38,8 +38,8 @@ def down(start,stop,ramp):
 
 def main():
     while True:
-        #up(0,3000,5)
-        down(3000,0,5)
+        up(0,6000,5,1)
+        down(6000,0,5,2)
 
 if __name__ == '__main__':
     main()
